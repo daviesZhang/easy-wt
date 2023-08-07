8b1df5f0209f8f0b591fbd21591ef7bf80e36765
davies <sychopian@gmail.com>
2023-08-07 12:11:50 +0800

    暗黑样式微调

--- packages/ui-shared/src/lib/utils/utils.ts
+++ packages/ui-shared/src/lib/utils/utils.ts
@@ -3,8 +3,5 @@
  * @param value
  */
 export function paramsHtml(value: string): string {
-  return value.replace(
-    /(\$\{.+?})/g,
-    `<sapn style="color: #663399;font-weight: bold;font-style: oblique;">$1</sapn>`
-  );
+  return value.replace(/(\$\{.+?})/g, `<sapn class="step-variable">$1</sapn>`);
 }
